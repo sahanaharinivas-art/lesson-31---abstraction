7ef9240f20f0c50805b00fc70252f03ee1df8b81
harinivassahana-max <harinivassahana@gmail.com>
2026-02-17 16:14:17 +1100

    activity 1

---
+++ activity 1.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class Absclass(ABC):
+
+    def print(self, x):
+        print("passed value:", x)
+
+    @abstractmethod
+    def task(self):
+        print("we are inside Absclass task")
+
+class test_class(Absclass):
+    def task(self):
+        print("we are inside test_class task")
+
+test_obj = test_class()
+test_obj.task()
+test_obj.print(100)
+
